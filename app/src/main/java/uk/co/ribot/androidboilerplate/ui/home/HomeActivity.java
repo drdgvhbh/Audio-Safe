c51750a71f8b265e2c8ecf04beedde10c2e0c0c4
shay059 <spencer.hayes.laverdiere@gmail.com>
2018-02-18 06:07:06 -0500

    Spicy commit numero 4

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/home/HomeActivity.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/home/HomeActivity.java
@@ -1,20 +1,37 @@
 package uk.co.ribot.androidboilerplate.ui.home;
 
 import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceView;
 import android.view.View;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import uk.co.ribot.androidboilerplate.R;
 import uk.co.ribot.androidboilerplate.StatsActivity;
 import uk.co.ribot.androidboilerplate.ui.heatmap.HeatMapActivity;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
 
     public void onButtonMap(View view){
